847fed38c6869daf5a4a1ebc93e6bbcf8cf2b984
gleybsonlopes <gleybsonlopes91@gmail.com>
2023-03-16 10:39:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Orgs-Listagem-de-Produtos-Kotlin
+ Aplicativo Android com Kotlin para Cadastro, Listagem, Exclusão com Room
---
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -0,0 +1,26 @@
+package br.com.alura.orgs.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import br.com.alura.orgs.model.Produto
+
+@Dao
+interface ProdutoDao {
+
+    @Query("SELECT * FROM Produto")
+    fun buscaTodos() : List<Produto>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun salva(vararg produto: Produto)
+
+    @Delete
+    fun remove(produto: Produto)
+
+    @Query("SELECT * FROM Produto WHERE id = :id")
+    fun buscaPorId(id: Long) : Produto?
+
+}
